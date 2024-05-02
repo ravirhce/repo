d724a0e177b3a576b0038a716a40ded7ffbe3fba
ravindra <ravi.rhce@gmail.com>
2024-05-02 09:16:03 +0000

    added container variable

--- a1.provider.tf
+++ a1.provider.tf
@@ -16,8 +16,26 @@ terraform {
 
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  #skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+tenant_id = var.tenant_id
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
 }
